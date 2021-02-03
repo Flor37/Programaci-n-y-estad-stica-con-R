7012606bf6d89a856d3bc29b8ecd907a4c0c6ed3
Flor37 <77375979+Flor37@users.noreply.github.com>
2021-02-02 23:39:04 -0600

    Update POSTWORKsesion3FLORMEDINA.R

--- POSTWORKsesion3FLORMEDINA.R
+++ POSTWORKsesion3FLORMEDINA.R
@@ -1,7 +1,7 @@
-#Ahora graficaremos probabilidades (estimadas) marginales y conjuntas para el número de goles que anotan 
+#Ahora graficaremos probabilidades (estimadas) marginales y conjuntas para el nÃºmero de goles que anotan 
 #en un partido el equipo de casa o el equipo visitante.
 
-# 1.Con el último data frame obtenido en el postwork de la sesión 2, elabora tablas de frecuencias relativas 
+# 1.Con el Ãºltimo data frame obtenido en el postwork de la sesiÃ³n 2, elabora tablas de frecuencias relativas 
 #   para estimar las siguientes probabilidades:
 library(ggplot2)
 library(dplyr)
@@ -15,38 +15,38 @@ FTAG <- liga$FTAG
 
 x <- c(FTGH)
 totg.c <- table(x)
-pm.c <- prop.table(pm.c)
+pm.c <- prop.table(totg.c)
 
 # -La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y=0,1,2,)
 
 y <- c(FTAG)
 totg.v <- table(y)
-pm.v <- prop.table(pm.v)
+pm.v <- prop.table(totg.v)
 
 # -La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles (x=0,1,2,, y=0,1,2,)
 
 totg.cv <- table(x, y)
-pc <- prop.table(pc.cv)
+pc <- prop.table(totg.cv)
 
 # 2.Realiza lo siguiente:
-# -Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo 
+# -Un grÃ¡fico de barras para las probabilidades marginales estimadas del nÃºmero de goles que anota el equipo 
 #   de casa
 
 barplot(pm.c, main = "PM de los goles anotados por equipos locales",
-        xlab = "Número de goles",
+        xlab = "NÃºmero de goles",
         ylab = "Probabilidad Marginal",
         col = c("blue","royalblue", "lightblue"))
 
-# -Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo 
+# -Un grÃ¡fico de barras para las probabilidades marginales estimadas del nÃºmero de goles que anota el equipo 
 #   visitante.
 
 barplot(pm.v, main = "PM de los goles anotados por equipos visitantes",
-        xlab = "Número de goles",
+        xlab = "NÃºmero de goles",
         ylab = "Probabilidad Marginal",
         col = c("green","lightgreen", "gray"))
 
 
-# -Un HeatMap para las probabilidades conjuntas estimadas de los números de goles que anotan el equipo de 
+# -Un HeatMap para las probabilidades conjuntas estimadas de los nÃºmeros de goles que anotan el equipo de 
 #   casa y el equipo visitante en un partido.
 
 heatmap(pc, main = "Probabilidad conjunta",
